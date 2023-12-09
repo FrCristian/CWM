af8d29a80dbcdf390167049338d9800f1190e7a6
FrCristian <144194163+FrCristian@users.noreply.github.com>
2023-12-08 19:59:20 -0500

    actualizacion

--- E-commerce/main.js
+++ E-commerce/main.js
@@ -58,8 +58,32 @@ $(function(){
         }
     });
 
-    
 
+    // Obtén el modal
+   var modal = document.getElementById("aboutModal");
+
+    // Obtén el botón que abre el modal
+   var btn = document.getElementById("aboutBtn");
+
+   // Obtén el elemento <span> que cierra el modal
+   var span = document.getElementsByClassName("close")[0];
+
+   // Cuando el usuario haga clic en el botón, abre el modal 
+   btn.onclick = function(event) {
+   event.preventDefault(); // Evita que el enlace haga scroll a la sección "#About"
+   modal.style.display = "block";
+   }
+
+   // Cuando el usuario haga clic en <span> (x), cierra el modal
+   span.onclick = function() {
+   modal.style.display = "none";
+   }  
+   // Cuando el usuario haga clic en cualquier lugar fuera del modal, cierra el modal
+   window.onclick = function(event) {
+   if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 });
 
 
